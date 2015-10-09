29ee45ce351b15c9aa906914b1a2738f48c7885d
Paulo Alem <paulo.alem@corp.globo.com>
2015-10-09 10:08:25 -0300

    Pleasing brew audit

--- wrk2.rb
+++ wrk2.rb
@@ -1,10 +1,17 @@
 class Wrk2 < Formula
-  head "https://github.com/giltene/wrk2.git"
+  desc "Constant throughput, correct latency recording variant of wrk"
   homepage "https://github.com/giltene/wrk2"
+  head "https://github.com/giltene/wrk2.git"
+
+  depends_on "openssl"
 
   def install
     system "make"
     mv "wrk", "wrk2"
     bin.install ["wrk2"]
+  end
+
+  test do
+    system "#{bin}/wrk2"
   end
 end
